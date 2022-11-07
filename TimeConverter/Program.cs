14f0d0dc593b64665adb837f9fd9afa6f1c245d5
RealNullAndVoid <thenewgamerpro76@gmail.com>
2022-11-07 12:09:38 -0500

    Add project files.

---
+++ TimeConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TimeConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int UTC = -5;
+            var dt = DateTime.Now;
+            var dttime = dt.ToString("H:mm");
+            var dtinttime = dt.ToUniversalTime();
+            var dtinttime2 = dt.AddHours(-5);
+            Console.WriteLine(dt);
+            Console.WriteLine(dttime);
+            Console.WriteLine(dtinttime);
+            Console.WriteLine(dtinttime2);
+
+        }
+    }
+}
